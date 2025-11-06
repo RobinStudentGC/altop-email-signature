34acb8b2c84d2167e33663396e43a897d1432c00
Robin Peter Miljoen <robin.miljoen@student.graafschapcollege.nl>
2025-11-06 10:08:17 +0100

    base64 testing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,7 @@
 
 <body
   style="font-family: Arial, Helvetica, sans-serif; font-size: 18px; margin: 0; padding: 0; box-sizing: border-box; background: #f6f6f6;">
+
   <div class="handtekening-block" style="width: 100%; box-sizing: border-box;">
     <div class="handtekening-verzender-content"
       style="width: 100%; display: flex; justify-content: space-between; flex-wrap: wrap;">
@@ -18,13 +19,13 @@
 
       <div class="logo-cell"
         style="width: 100%; max-width: 200px; display: flex; align-items: center; justify-content: center; padding: 0; box-sizing: border-box;">
-        <img src="http://docker07.ap.altop:5194/images/Logo%20AltopT.png" alt="Altop Logo" class="logo-img"
+        <img class="base-img" data-src="http://docker07.ap.altop:5194/images/Logo%20AltopT.png" alt="Altop Logo"
           style="max-width: 200px; width: 100%; height: auto; display: block; margin: auto; object-fit: contain; padding-right: 10px;" />
       </div>
     </div>
 
     <div class="balk" style="width: 100%;">
-      <img class="balk-img" src="http://docker07.ap.altop:5194/images/Balk.png" alt=""
+      <img class="base-img" data-src="http://docker07.ap.altop:5194/images/Balk.png" alt=""
         style="width: 100%; display: block; margin: auto; height: auto;" />
     </div>
 
@@ -32,8 +33,12 @@
       (telefoonnummer verzender)
     </div>
 
-    <div class="informatie-container" style="position: relative; width: 100%; height: 168px; box-sizing: border-box;">
-      <div class="background-img" style="position: absolute; top: 0; left: 0; right: 0; width: 100%; height: 100%; background-image: url('http://docker07.ap.altop:5194/images/AdresVlak.png'); background-size: auto; background-color: transparent; background-position: right top; background-repeat: no-repeat; z-index: 1; pointer-events: none;"></div>
+    <div class="informatie-container"
+      style="position: relative; width: 100%; height: 168px; box-sizing: border-box;">
+      <div class="background-img"
+        data-src="http://docker07.ap.altop:5194/images/AdresVlak.png"
+        style="position: absolute; top: 0; left: 0; right: 0; width: 100%; height: 100%; background-image: url(''); background-size: auto; background-color: transparent; background-position: right top; background-repeat: no-repeat; z-index: 1; pointer-events: none;">
+      </div>
       <div class="informatie-block" style="font-size: 1em; position: relative; z-index: 2; color: white;">
         Stirlingstraat 4<br />
         7037 DG Beek (Montferland)<br />
@@ -42,4 +47,46 @@
       </div>
     </div>
   </div>
+
+  <script>
+    async function toBase64(url) {
+      const res = await fetch(url);
+      const blob = await res.blob();
+      return new Promise(resolve => {
+        const reader = new FileReader();
+        reader.onloadend = () => resolve(reader.result);
+        reader.readAsDataURL(blob);
+      });
+    }
+
+    async function convertImages() {
+      // Convert all <img> tags with class "base-img"
+      const imgs = document.querySelectorAll('.base-img');
+      for (let img of imgs) {
+        const src = img.dataset.src;
+        try {
+          const base64 = await toBase64(src);
+          img.src = base64;
+        } catch (e) {
+          console.error("Failed to convert", src, e);
+        }
+      }
+
+      // Convert all background images with data-src
+      const bgs = document.querySelectorAll('[data-src]');
+      for (let bg of bgs) {
+        if (bg.tagName === "DIV") {
+          const src = bg.dataset.src;
+          try {
+            const base64 = await toBase64(src);
+            bg.style.backgroundImage = `url('${base64}')`;
+          } catch (e) {
+            console.error("Failed to convert background", src, e);
+          }
+        }
+      }
+    }
+
+    window.addEventListener('load', convertImages);
+  </script>
 </body>
