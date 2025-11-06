edc4f72077c6989393bef2616259b22dfb849ac5
Robin Peter Miljoen <robin.miljoen@student.graafschapcollege.nl>
2025-11-06 09:33:48 +0100

    images added and docker changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+</head>
+
+<body
+  style="font-family: Arial, Helvetica, sans-serif; font-size: 18px; margin: 0; padding: 0; box-sizing: border-box; background: #f6f6f6;">
+  <div class="handtekening-block" style="width: 100%; box-sizing: border-box;">
+    <div class="handtekening-verzender-content"
+      style="width: 100%; display: flex; justify-content: space-between; flex-wrap: wrap;">
+      <div class="verzender-cell" style="padding: 10px 20px 10px 20px; font-size: 1em; box-sizing: border-box;">
+        Met vriendelijke groet,<br /><br />
+        <strong>(Naam verzender)</strong><br />
+        (functie)<br />
+        <br /><br /><br />
+      </div>
+
+      <div class="logo-cell"
+        style="width: 100%; max-width: 200px; display: flex; align-items: center; justify-content: center; padding: 0; box-sizing: border-box;">
+        <img src="src/lib/assets/images/Logo%20AltopT.png" alt="Altop Logo" class="logo-img"
+          style="max-width: 200px; width: 100%; height: auto; display: block; margin: auto; object-fit: contain; padding-right: 10px;" />
+      </div>
+    </div>
+
+    <div class="balk" style="width: 100%;">
+      <img class="balk-img" src="src/lib/assets/images/Balk.png" alt=""
+        style="width: 100%; display: block; margin: auto; height: auto;" />
+    </div>
+
+    <div class="verzender-nummer" style="padding: 10px 20px; font-size: 1em; box-sizing: border-box;">
+      (telefoonnummer verzender)
+    </div>
+
+    <div class="informatie-container" style="position: relative; width: 100%; height: 168px; box-sizing: border-box;">
+  <div class="background-img" style="position: absolute; top: 0; left: 0; right: 0; width: 100%; height: 100%; background-image: url('src/lib/assets/images/AdresVlak.png'); background-size: auto; background-color: transparent; background-position: right top; background-repeat: no-repeat; z-index: 1; pointer-events: none;"></div>
+        <div class="informatie-block" style="font-size: 1em; position: relative; z-index: 2; color: white;">
+        Stirlingstraat 4<br />
+        7037 DG Beek (Montferland)<br />
+        +31 (0)316 82 00 20<br />
+        www.altoptechnologies.com<br />
+      </div>
+    </div>
+  </div>
+</body>
